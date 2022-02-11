e93092e859575d463a2759d602e8b8c4792f62c3
invisibleeinstei <91374376+invisibleeinstei@users.noreply.github.com>
2022-02-11 17:00:34 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -502,7 +502,7 @@ async def not_chat(_, m: Message):
         buttons = [
             [
                 InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-                InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+                InlineKeyboardButton('ᴀʟ ᴋᴜᴘᴘɪʏᴀ ɢʀᴏᴜᴘ 💡', url='https://t.me/apealkuppiya'),
             ]
             ]
         await m.reply("<b>You can't use this bot in this group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/VCPlayerBot) below.</b>", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))
